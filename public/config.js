7b90bf34b7d2f56417d0d148a53680e793ddd021
CryptedEx <105995404+CryptedEx@users.noreply.github.com>
2023-08-11 22:19:25 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
   VITE_CORS_PROXY_URL: "https://hello-world-late-frog-c258.0nlyex-vfx.workers.dev",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMmIwMjJiNTljMjg1OTI5Y2QyYzhjNDc2MTYwMjZmNiIsInN1YiI6IjY0ZDZhM2UyZGI0ZWQ2MDBlMmI2ODk0NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.9ulgtkCW_0Fj3kx-8Qjs8rU3uP9yNvh_yN2BDVYCzZE"
 };
