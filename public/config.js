4a113e85cc16306b11a8b71295f684706c473f5e
CryptedEx <105995404+CryptedEx@users.noreply.github.com>
2023-08-11 22:14:56 +0100

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
+  VITE_CORS_PROXY_URL: "https://hello-world-late-frog-c258.0nlyex-vfx.workers.dev",
   VITE_TMDB_READ_API_KEY: ""
 };
